654a567792546a5e2412b22deb4cc161101f8a85
not-a-no <19555822+not-a-no@users.noreply.github.com>
2024-03-14 22:10:05 -0400

    edit numbers_in_python

---
+++ numbers_in_python.py
@@ -0,0 +1,14 @@
+#group digits using underscrores to make large numbers more readable
+universe_age = 14_000_000_000
+
+print(universe_age)
+
+#multiple assignment
+x, y, z = 0, 0, 0
+
+"""Python doesn't have built-in constant types, 
+but Python programmers use all capital letters 
+to indicate a varible should be treated as 
+a constant and never be changed"""
+
+
